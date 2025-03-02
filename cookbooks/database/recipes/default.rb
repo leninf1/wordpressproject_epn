46fddf853b8364d4e9ded583b4c273017575c1d4
Lenin Mora Ojeda <lmora@cmac-cusco.com.pe>
2025-03-02 17:12:17 -0500

    Aprovisionamiento SO database
    Configuración del aprovisionamiento de las actualizaciones del SO, evaluando el sistema operativo anfitrión

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2025, The Authors, All Rights Reserved.
+case node['platform_family']
+when 'debian', 'ubuntu'
+    execute "update" do
+        command "apt update -y && apt upgrade -y"
+        action :run
+    end
+    include_recipe 'database::ubuntu'
+when 'rhel', 'fedora'
+    execute "update" do
+        command "sudo dnf update -y && sudo dnf upgrade -y"
+        action :run
+    end
+    include_recipe 'database::centos'
+end
